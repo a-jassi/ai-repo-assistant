2ab8fa94858057186ce9b41648409ed87cdbd321
Arshdeep Jassi <arshdeepjassi1234@gmail.com>
2025-01-02 11:55:02 -0800

    Format project.ts

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -2,37 +2,39 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const projectRouter = createTRPCRouter({
-  createProject: protectedProcedure.input(
-    z.object({
-      projectName: z.string(),
-      githubUrl: z.string(),
-      githubToken: z.string().optional(),
-    })
-  ).mutation(async ({ctx, input}) => {
-    const project = await ctx.db.project.create({
-      data: {
-        name: input.projectName,
-        githubUrl: input.githubUrl,
-        users: {
-          create: {
-            userId: ctx.user.userId!,
-          }
-        }
-      }
-    })
+  createProject: protectedProcedure
+    .input(
+      z.object({
+        projectName: z.string(),
+        githubUrl: z.string(),
+        githubToken: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const project = await ctx.db.project.create({
+        data: {
+          name: input.projectName,
+          githubUrl: input.githubUrl,
+          users: {
+            create: {
+              userId: ctx.user.userId!,
+            },
+          },
+        },
+      });
 
-    return project
-  }),
+      return project;
+    }),
   getProjects: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.project.findMany({
       where: {
         users: {
-          some : {
-            userId: ctx.user.userId!
-          }
+          some: {
+            userId: ctx.user.userId!,
+          },
         },
-        deletedAt: null
-      }
-    })
-  })
-})
+        deletedAt: null,
+      },
+    });
+  }),
+});
