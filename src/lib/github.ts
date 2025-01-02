f4a3d671605d8b108c46a3122ac2cac1c78d6805
Arshdeep Jassi <arshdeepjassi1234@gmail.com>
2025-01-02 11:29:50 -0800

    Formatted github.ts with prettier

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -3,9 +3,9 @@ import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
-})
+});
 
-const githubUrl = 'https://github.com/a-jassi/ai-repo-assistant'
+const githubUrl = "https://github.com/a-jassi/ai-repo-assistant";
 
 type Response = {
   commitHash: string;
@@ -13,71 +13,80 @@ type Response = {
   commitAuthorName: string;
   commitAuthorAvatar: string;
   commitDate: string;
-}
+};
 
-export const getCommitHashes = async (githubUrl: string): Promise<Response[]> => {
-  const [owner, repo] = githubUrl.split('/').slice(-2)
+export const getCommitHashes = async (
+  githubUrl: string,
+): Promise<Response[]> => {
+  const [owner, repo] = githubUrl.split("/").slice(-2);
   if (!owner || !repo) {
-    throw new Error("Invalid GitHub URL")
+    throw new Error("Invalid GitHub URL");
   }
 
   const { data } = await octokit.rest.repos.listCommits({
     owner,
     repo,
-  })
+  });
 
-  const sortedCommits = data.sort((a: any, b: any) => (
-    new Date(b.commit.author.date).getTime() - new Date(a.commit.author.date).getTime()
-  ))
+  const sortedCommits = data.sort(
+    (a: any, b: any) =>
+      new Date(b.commit.author.date).getTime() -
+      new Date(a.commit.author.date).getTime(),
+  );
 
   return sortedCommits.slice(0, 10).map((commit: any) => ({
     commitHash: commit.sha as string,
-    commitMessage: commit.commit?.message ?? '',
-    commitAuthorName: commit.commit?.author?.name ?? '',
-    commitAuthorAvatar: commit.commit?.author?.avatar_url ?? '',
-    commitDate: commit.commit?.author.date ?? '',
-  }))
-}
+    commitMessage: commit.commit?.message ?? "",
+    commitAuthorName: commit.commit?.author?.name ?? "",
+    commitAuthorAvatar: commit.commit?.author?.avatar_url ?? "",
+    commitDate: commit.commit?.author.date ?? "",
+  }));
+};
 
 const fetchProjectGithubUrl = async (projectId: string) => {
   const project = await db.project.findUnique({
     where: { id: projectId },
     select: {
-      githubUrl: true
-    }
-  })
+      githubUrl: true,
+    },
+  });
 
   if (!project?.githubUrl) {
-    throw new Error("Project has no GitHub URL")
+    throw new Error("Project has no GitHub URL");
   }
 
   return {
     project,
-    githubUrl: project?.githubUrl
-  }
-}
+    githubUrl: project?.githubUrl,
+  };
+};
 
-const filterUnprocessedCommits = async (projectId: string, commitHashes: Response[]) => {
+const filterUnprocessedCommits = async (
+  projectId: string,
+  commitHashes: Response[],
+) => {
   const processedCommits = await db.commit.findMany({
-    where: { projectId }
+    where: { projectId },
   });
-  
-  const processedCommitHashes = new Set(processedCommits.map((commit) => commit.commitHash));
-  
-  const unprocessedCommits = commitHashes.filter((commit) => 
-    !processedCommitHashes.has(commit.commitHash)
+
+  const processedCommitHashes = new Set(
+    processedCommits.map((commit) => commit.commitHash),
   );
-  
+
+  const unprocessedCommits = commitHashes.filter(
+    (commit) => !processedCommitHashes.has(commit.commitHash),
+  );
+
   return unprocessedCommits;
-  
-}
+};
 
-const summarizeCommits = async (githubUrl: string, commitHash: string) => {
-
-}
+const summarizeCommit = async (githubUrl: string, commitHash: string) => {};
 
 export const pollCommits = async (projectId: string) => {
-  const { project, githubUrl } = await fetchProjectGithubUrl(projectId)
-  const commitHashes = await getCommitHashes(githubUrl)
-  const unprocessedCommits = await filterUnprocessedCommits(projectId, commitHashes)
-}
+  const { project, githubUrl } = await fetchProjectGithubUrl(projectId);
+  const commitHashes = await getCommitHashes(githubUrl);
+  const unprocessedCommits = await filterUnprocessedCommits(
+    projectId,
+    commitHashes,
+  );
+};
